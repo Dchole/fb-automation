6c6cdaa21738eb61213e8c05c2fe860417c22eec
Dchole <derekoware47@gmail.com>
2021-11-25 00:28:05 +0000

    scroll

--- src/index.js
+++ src/index.js
@@ -36,21 +36,30 @@ const href = "https://web.facebook.com"
         `div[aria-posinset="${postCount}"]`
       )
 
-      const [likeButton, commentButton, adLink] = await Promise.all([
-        post.$('div[aria-label="Like"]'),
-        post.$('div[aria-label="Like"]'),
+      const { height } = await post.boundingBox()
+      await page.mouse.wheel({ deltaY: height })
+
+      const [author, refAuthor, adLink] = await Promise.all([
+        post.$("h4"),
+        post.$("h5"),
         post.$('a[aria-label="Sponsored"]')
       ])
 
-      if (likeButton && commentButton && !adLink) {
-        await likeButton.click()
-        await commentButton.click()
+      if ((author || refAuthor) && !adLink) {
+        await post.$('div[aria-label="Like"]')
+        // .then(likeButton => likeButton && likeButton.click())
+
+        await post
+          .$('div[aria-label="Leave a comment"]')
+          .then(commentButton => commentButton && commentButton.click())
 
         await page
-          .waitForSelector('div[aria-label="Write a comment"]')
+          .waitForSelector(
+            `div[aria-posinset="${postCount}"] div[aria-label="Write a comment"]`
+          )
           .then(commentInput => {
             commentInput.type("Hi").then(() => {
-              page.keyboard.press("Enter")
+              // page.keyboard.press("Enter")
             })
           })
       }
@@ -60,10 +69,10 @@ const href = "https://web.facebook.com"
       const loadedPosts = await page.$$('div[role="article"]')
 
       if (postCount === loadedPosts.length) {
-        loadedPosts
-          .at(-1)
+        loadedPosts[loadedPosts.length - 1]
           .$('div[aria-valuetext="Loading..."]')
-          .then(skeleton => {
+          .then(async skeleton => {
+            // End loop if no more posts are loading after the last post
             if (!skeleton) {
               scroll = false
             }
